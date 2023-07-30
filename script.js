98f214d3a75eb07a1eb1805847079cce5e16231f
Github15102002 <ankittiwari15102002@gmail.com>
2023-07-30 04:12:28 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function clearScreen(){
+    document.getElementById("result").value="";
+    }
+
+    function display(value){
+        document.getElementById("result").value += value;
+    }
+    function calculate(){
+        let p = document.getElementById("result").value;
+        let q=eval(p);
+        document.getElementById("result").value=q;
+    }
